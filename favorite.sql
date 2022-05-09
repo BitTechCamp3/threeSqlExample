a8b314dd3f400ff7ecc08696abec9e93a66756e4
Chris Jang <62946867+itsChrisJang@users.noreply.github.com>
2022-05-09 22:09:36 +0900

    Update favorite.sql

--- favorite.sql
+++ favorite.sql
@@ -2,8 +2,8 @@
 
 CREATE TABLE `favorites` (
   `index` int NOT NULL AUTO_INCREMENT,
-  `userid` varchar(45) DEFAULT NULL,
-  `coinsym` varchar(45) DEFAULT NULL,
+  `userId` varchar(45) DEFAULT NULL,
+  `coinSym` varchar(45) DEFAULT NULL,
   PRIMARY KEY (`index`),
   UNIQUE KEY `index_UNIQUE` (`index`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
