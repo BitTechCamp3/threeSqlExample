cfac92586b3e3e3820879ce8d51088b148dc398e
Chris Jang <62946867+itsChrisJang@users.noreply.github.com>
2022-05-09 22:09:52 +0900

    Update coin.sql

--- coin.sql
+++ coin.sql
@@ -2,9 +2,9 @@
 
 CREATE TABLE `coin` (
   `index` int NOT NULL AUTO_INCREMENT,
-  `coinsym` varchar(45) NOT NULL,
-  `coinname` varchar(45) DEFAULT NULL,
-  `useyn` tinyint DEFAULT '1',
+  `coinSym` varchar(45) NOT NULL,
+  `coinName` varchar(45) DEFAULT NULL,
+  `useYn` tinyint DEFAULT '1',
   PRIMARY KEY (`index`),
   UNIQUE KEY `index_UNIQUE` (`index`),
   UNIQUE KEY `coinsym_UNIQUE` (`coinsym`)
